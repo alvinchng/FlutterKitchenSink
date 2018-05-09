8bd70ceb84d6d60f4e84309de747e2be3e8b88e1
alvinchng <39010612+alvinchng@users.noreply.github.com>
2018-05-09 16:55:00 +0800

    Initial Commit

---
+++ lib/utilities/data_manager.dart
@@ -0,0 +1,16 @@
+
+
+class DataManager {
+
+  static final DataManager _singleton = new DataManager._internal();
+
+  factory DataManager() {
+    return _singleton;
+  }
+
+  DataManager._internal();
+
+  // Methods
+  
+
+}
